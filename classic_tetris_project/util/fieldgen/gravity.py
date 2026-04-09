bb20af7371e7755d29858e56bc4c804d1e82d16f
Sean <74142852+mathmaster13@users.noreply.github.com>
2026-04-09 13:24:52 -0400

    PAL level table for later

--- classic_tetris_project/util/fieldgen/gravity.py
+++ classic_tetris_project/util/fieldgen/gravity.py
@@ -1,5 +1,5 @@
 class GravityFrames(object):
-    LEVELS = [
+    LEVELS_NTSC = [
         48,
         43,
         38,
@@ -26,6 +26,28 @@ class GravityFrames(object):
         2,
         2,
         2,
+        2,
+        2,
+        2,
+    ]
+
+    LEVELS_PAL = [
+        36,
+        32,
+        29,
+        25,
+        22,
+        18,
+        15,
+        11,
+        7,
+        5,
+        4,
+        4,
+        4,
+        3,
+        3,
+        3,
         2,
         2,
         2,
